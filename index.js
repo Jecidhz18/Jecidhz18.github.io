9c549d0cce9f611f977ebc41e25274577f72df97
Jecidhz18 <57949609+Jecidhz18@users.noreply.github.com>
2024-04-23 19:08:51 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,114 @@
+let dimensions = {
+    x: "",
+    y: ""
+}
+let images = [
+    "https://media.giphy.com/media/l2R0cE5EqO3QHiCoU/giphy.gif",
+    "https://media.giphy.com/media/Saavhnp9YYN7a/giphy.gif",
+    "https://media.giphy.com/media/26xBRiIYbyjCzYMAU/giphy.gif",
+    "https://media.giphy.com/media/l1AsHO8u4GHnD3DOg/giphy.gif",
+    "https://media.giphy.com/media/26tPplGWjN0xLybiU/giphy.gif",
+    "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif",
+    "https://media.giphy.com/media/JpGCB3Wk8hWjIAR9Cz/giphy.gif",
+    "https://media.giphy.com/media/JpGCB3Wk8hWjIAR9Cz/giphy.gif",
+    "https://media.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif",
+    "https://media.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif",
+    "https://media.giphy.com/media/1x3LVhXaUdISA/giphy.gif",
+    "https://media.giphy.com/media/gjC7ucoTg5AkHGcy3J/giphy.gif",
+
+]
+let $contenedor = document.querySelector("#contenedor-eleccion");
+let $btnNo = document.querySelector("#contenedor-eleccion");
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+    };
+}
+const getRandomNumber = (size) => {
+    return Math.floor(Math.random() * size);
+}
+const setDimensions = () => {
+    dimensions.y = $contenedor.offsetHeight;
+    dimensions.x = $contenedor.offsetWidth;
+    // console.log(dimensions);
+}
+const getDimensionsBody = () => {
+    return {
+        x: document.querySelector("body").offsetHeight,
+        y: document.querySelector("body").offsetWidth,
+    }
+}
+const translateButton = (e) => {
+    // let dimensionsBody = getDimensionsBody();
+    let x = getRandomNumber(dimensions.x / 2.8);
+    let y = getRandomNumber(dimensions.y / 2);
+    // console.log(x, y);
+    // console.log(x, dimensionsBody.x);
+    // if (x > dimensionsBody.x || y > dimensionsBody.y) {
+    //     translateButton(e);
+    // } else {
+    e.target.style.transform = `translate(${x}px,${y}px)`;
+    // }
+}
+window.onload = () => {
+    setDimensions();
+
+}
+const reproducirMusica = () => {
+    let music = document.getElementById("audio");
+    music.play();
+}
+document.addEventListener("DOMContentLoaded", () => {
+
+
+});
+window.onresize = () => {
+    setDimensions();
+    document.querySelector("#btn-no").style.transform = "none";
+}
+
+document.querySelector("#btn-no").addEventListener("mouseover", (e) => {
+    // detecto el button
+    translateButton(e);
+});
+document.querySelector("#btn-no").addEventListener("click", (e) => {
+    // detecto el button
+    translateButton(e);
+});
+document.querySelector("#btn-no").addEventListener("focus", (e) => {
+    // detecto el button
+    translateButton(e);
+});
+document.querySelector("#btn-no").addEventListener("blur", (e) => {
+    // detecto el button
+    translateButton(e);
+});
+document.querySelector("#btn-si").addEventListener("click", (e) => {
+    reproducirMusica();
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'w-100 p-3 mx-2 my-2  transition duration-500 transform hover:scale-110 md:p-4 md:my-4  md:text-xl md:mx-4 color-primary bg-pink-400 border-none rounded-lg shadow-xl',
+        },
+        buttonsStyling: false
+    })
+    swalWithBootstrapButtons.fire({
+
+        title: `<h2 class='color-secondary'>Ya Sabia que querías</h2>`,
+        // icon: 'info',
+        html: `
+            <img src="${images[getRandomNumber(images.length)]}" alt="" srcset="" class='gift-image'/>
+        `,
+        showCloseButton: false,
+        showCancelButton: false,
+        focusConfirm: false,
+        confirmButtonText:
+            'Vuelve a elegir si quieres',
+    }).then(function () {
+        document.querySelector("#btn-no").style.transform = "none";
+    })
+});
+
+
+// document.addEventListener
